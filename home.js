6651adf1e11a5da7604477a66326f3b357ae848b
Fr0msea <marinho.felipethomas@gmail.com>
2022-03-10 10:45:53 -0300

    Mudança na tonalidade

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-const botao = document.getElementById('btn')
+/*const botao = document.getElementById('btn')
 
 botao.addEventListener('click',menuMobile)
 
@@ -6,4 +6,29 @@ function menuMobile() {
     const nav = document.getElementById('nav')
 
     nav.classList.toggle('active')
+}*/
+/*
+const $menu = document.querySelector('.menu')
+const $navBar = document.querySelectorAll('.nav-bar')[0]
+var isOpen = false
+$menu.addEventListener('click',toggleMenu)
+
+function toggleMenu(){
+    $navBar.classList.toggle('menu-opened');
+
+    if (isOpen==false) {
+        $navBar.classList.add('menu-opened')
+        isOpen = true
+    }else{
+        $navBar.classList.remove('menu-opened')
+        isOpen = false
+    }
 }
+*/
+const btn = document.querySelector('.menu')
+            const $navBar = document.querySelector('.nav-bar')
+            btn.addEventListener('click',toggleMenu)
+
+            function toggleMenu() {
+                $navBar.classList.toggle('sumi')
+            }
